c850bd15bdbdfea94d0bd833bc62dddfbbc42bb4
cloiss <thomaskent621@gmail.com>
2020-10-08 19:18:18 -0400

    Add drive and angle test code from user/cloiss/drivecallib branch

--- src/scripts/blue.cpp
+++ src/scripts/blue.cpp
@@ -1,4 +1,9 @@
 #include "main.h"
 
 void blue() {
+  for (int i=0; i < 4 ;i++)
+  {
+    drive(2);
+    delay(1000);
+  }
 }
--- src/scripts/red.cpp
+++ src/scripts/red.cpp
@@ -1,4 +1,10 @@
 #include "main.h"
 
 void red() {
+  for (int i=0; i < 4 ;i++)
+  {
+    turn(90);
+    delay(1000);
+  }
+  drive(2);
 }
