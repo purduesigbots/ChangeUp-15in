6c56405fc9ad45232b8c532fec782efe63354a96
Jake Rowell <jscottr557@gmail.com>
2020-11-04 22:22:24 -0500

    Fix Auton for compile

--- src/scripts/red.cpp
+++ src/scripts/red.cpp
@@ -4,7 +4,7 @@ void red() {
 	// auton to pick up ball #1
 
 	//commented for testing/tuning turn function
-	/*chassis::move(28, 50);
+	chassis::move(28, 50);
   intake::move(100);
   chassis::move(28, 50);
 }
