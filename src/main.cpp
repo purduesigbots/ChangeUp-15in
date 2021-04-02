22ac5b105e56522921247c48dd33c2fa0c52b615
Micah Rassi <mrassi@purdue.edu>
2021-04-01 20:12:35 -0400

    Upgrade pros kernel

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,52 @@
 pros::Controller master(CONTROLLER_MASTER);
 
 void initialize() {
-	// autonomous selector library
 	selector::init();
 
-	chassis::init({-15, -18},  //Left motors
-	          {17,19},  //Right motors
-	          200, // gearset
-						45.0,  //distance constant
-	          4.75,  //degree constant
-	          3, //acccel, step
-						2, //arc step
-	          .2, .75,  //linear kp, kd
-	          1, 6,  //turn kp, kd
-	          .05, //arc kp
-						1, //dif kp
-						0,  //IMU port
-	          {1,3,0},  //encoder ports
-	          0);  //expander port
+	chassis::init({-15, -18}, {17, 19}, // motors
+	              600,                  // gearset
+	              45, 4.75,             // TPU
+	              12,                   // setle time
+	              6, 5,                 // linear/angular thresholds
+	              2, 2,                 // regular/arc slew
+	              16,                   // imu port
+	              {1, 3, 0},            // encoder ports
+	              0,                    // expander port
+	              10                    // joystick threshold
+	);
+	odom::init(false, // debug output
+	           7.825, // left/right distance
+	           7.825, // middle distance
+	           69.44, // left/right tpi
+	           69.44, // middle tpi
+	           true,  // holonomic enabled
+	           2      // exit error
+
+	);
+	pid::init(false,  // debug output
+	          .3, .5, // linear constants
+	          1.4, 3, // angular contants
+	          4, 0,   // linear point constants
+	          50, 0,  // angular point constants
+	          .05,    // arc kp
+	          .5,     // dif kp
+	          10      // min error
+	);
+
+	chassis::init({-15, -18}, // Left motors
+	              {17, 19},   // Right motors
+	              200,        // gearset
+	              45.0,       // distance constant
+	              4.75,       // degree constant
+	              3,          // acccel, step
+	              2,          // arc step
+	              .2, .75,    // linear kp, kd
+	              1, 6,       // turn kp, kd
+	              .05,        // arc kp
+	              1,          // dif kp
+	              0,          // IMU port
+	              {1, 3, 0},  // encoder ports
+	              0);         // expander port
 
 	// subsystems
 	intake::init();
